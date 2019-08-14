1d9e15dad92e19825e854c0c8f79f8db393e5914
Georgi Angelov <the.wizarda@gmail.com>
2019-08-14 10:33:57 +0300

    Add files via upload

---
+++ builder/frameworks/baremetal.py
@@ -0,0 +1,16 @@
+
+# WizIO 2018 Georgi Angelov
+# http://www.wizio.eu/
+# https://github.com/Wiz-IO
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = "baremetal"
+module = platform + "-" + env.BoardConfig().get("build.core")
+print "--MODULE", module
+m = __import__(module)       
+globals()[module] = m
+m.dev_init(env, platform)
+#print env.Dump()
+

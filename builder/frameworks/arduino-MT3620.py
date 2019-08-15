968314ea54fb6abf348bba86857e4e7e1976bf06
Georgi Angelov <the.wizarda@gmail.com>
2019-08-15 12:46:26 +0300

    Update arduino-MT3620.py

--- builder/frameworks/arduino-MT3620.py
+++ builder/frameworks/arduino-MT3620.py
@@ -24,6 +24,7 @@ def dev_init(env, platform):
     env.tool_dir = env.PioPlatform().get_package_dir("tool-azure")
     env.framework_dir = env.PioPlatform().get_package_dir("framework-azure")
     env.toolchain_dir = env.PioPlatform().get_package_dir("toolchain-arm-poky-linux-musleabi-hf")    
+    env.baremetal = False  
     dev_create_template(env)
     dev_compiler_poky(env)
     env.sysroot = env.BoardConfig().get("build.sysroot", "2")   # from ini file, default is 2 
